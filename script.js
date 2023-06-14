de637b01f58fceef8703046cb26c02c57368c926
Henry Li <62111018+yellowdragoon@users.noreply.github.com>
2023-06-14 22:21:56 +0100

    Complete design

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Testing');
+const passwordInput = document.querySelector('input#password');
+const confirmPasswordInput = document.querySelector('input#confirm-password');
+
+confirmPasswordInput.addEventListener('change', () => {
+    if(passwordInput.value !== confirmPasswordInput.value){
+        confirmPasswordInput.setCustomValidity('Oops, the passwords don\'t match!');
+    }
+    else{
+        confirmPasswordInput.setCustomValidity('');
+    }
+});
